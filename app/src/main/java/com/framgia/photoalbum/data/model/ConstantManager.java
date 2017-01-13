7b84d84bcbda4d79aba1b2dca0832e0259ce055c
Tran Dinh Sang <sangtran.bkdn@gmail.com>
2017-01-13 14:43:55 +0700

    capture image

---
+++ app/src/main/java/com/framgia/photoalbum/data/model/ConstantManager.java
@@ -0,0 +1,6 @@
+package com.framgia.photoalbum.data.model;
+
+public class ConstantManager {
+    public static final int REQUEST_TAKE_IMAGE = 1;
+    public static final String ARGUMENT_GET_IMAGE_BITMAP = "data";
+}
