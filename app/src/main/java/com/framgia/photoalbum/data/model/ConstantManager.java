104e207425b10e6ac56f751e3f0eef0c8af82b53
drtrungnguyen0306 <drtrungnguyen0306@gmail.com>
2017-01-18 07:59:26 +0700

    ref task-create-ui-scale-image

--- app/src/main/java/com/framgia/photoalbum/data/model/ConstantManager.java
+++ app/src/main/java/com/framgia/photoalbum/data/model/ConstantManager.java
@@ -2,6 +2,7 @@ package com.framgia.photoalbum.data.model;
 
 public class ConstantManager {
     public static final int REQUEST_TAKE_IMAGE = 1;
+    public static final int QUALYTY_IMAGE = 100;
     public static final String ARGUMENT_GET_IMAGE_BITMAP = "data";
     public static final String ARGUMENT_PUT_IMAGE_INTENT = "image";
 
